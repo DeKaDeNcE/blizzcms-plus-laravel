be79f2dc3551ad52d1f4c79176625067e3437006
Perioner <duelista16@gmail.com>
2019-10-27 15:41:18 +0100

    Theme/AquaFlameTribute : Uploaded

---
+++ public/themes/AquaFlameTribute/partials/sections/home.blade.php
@@ -0,0 +1,54 @@
+<div id="slider_wrapper">
+    <div id="slider">
+        <div id="slider_trickery">
+            <div id="slider_mask">
+                <!-- -->
+            </div>
+            <div class="flexslider">
+                <ul class="slides">
+                    <li><img src="{{ env('APP_URL')}}/themes/{{ config('theme.themeDefault') }}/assets/images/test/featured1.png" alt="" /></li>
+                    <li><img src="{{ env('APP_URL')}}/themes/{{ config('theme.themeDefault') }}/assets/images/test/featured2.png" alt="" /></li>
+                    <li><img src="{{ env('APP_URL')}}/themes/{{ config('theme.themeDefault') }}/assets/images/test/featured3.png" alt="" /></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div id="news_thumbs" class="clearfix">
+   	    <ul>
+            <li>
+                <a href="">
+                <img src="" alt="" style="background-image: url({{ env('APP_URL')}}/themes/{{ config('theme.themeDefault') }}/assets/images/test/newsthumb1.png);" />
+                <span>Demo AquaFlame</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                <img src="" alt="" style="background-image: url({{ env('APP_URL')}}/themes/{{ config('theme.themeDefault') }}/assets/images/test/newsthumb2.png);" />
+                <span>Demo AquaFlame</span>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                <img src="" alt="" style="background-image: url({{ env('APP_URL')}}/themes/{{ config('theme.themeDefault') }}/assets/images/test/newsthumb3.png);" />
+                <span>Demo AquaFlame</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+<div id="main_content">
+           <article>
+                	<header>
+                    	<h1><a href="">Demo AquaFlame</a></h1>
+                        <p class="meta">by <a href="">Beta</a> 2222 <a href="">2</a> <img src="{{ env('APP_URL')}}/themes/{{ config('theme.themeDefault') }}/assets/images/content-comments.png" alt="Comments" /></p>
+                    </header>
+                    <div class="content">
+                    	<div class="thumb">
+                        	<a href=""><img src="{{ env('APP_URL')}}/themes/{{ config('theme.themeDefault') }}/assets/images/test/newsthumb2.png" alt="" /></a>
+                        </div>
+                    	<p>Demo AquaFlame noticias</p>
+						<div align="right"><a href="" class="login-btn">Read More</a></div>
+                    </div>
+                    <footer><!-- --></footer>
+            </article>
+</div>
